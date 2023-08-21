b4f2cf1b10d78cb2a6663cabb00ba7bfd6d0419e
anthal <anthal@web.de>
2023-08-21 21:13:30 +0200

    ADD MicroPython files

---
+++ MicroPython/settings.py
@@ -0,0 +1,21 @@
+net = {
+    'ssid': 'your ssid',
+    'key': 'your key',
+    'country': 'DE',
+    }
+
+mqtt = {
+    'mqttBroker' = '192.168.0.111',
+    'mqttClient' = 'RasPiPico',
+    'mqttUser' = 'mqttUser',
+    'mqttPW' = 'mqttPW',
+    'mqttTopic' = b"mqttTopic"
+    }
+
+time = {
+    'ntp_server': 'pool.ntp.org',
+    # Sommerzeit:
+    'gmt_offset': '2'
+    # Winterzeit:
+    #'gmt_offset': '1'
+    }
